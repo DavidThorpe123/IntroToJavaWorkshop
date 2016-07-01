d48c4960b448859d2eeecfec73a049ec80a50666
Love4653 <Kaiya.park@gmail.com>
2016-07-01 14:56:16 -0700

    cool stuff

--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -1,6 +1,7 @@
 package day5;
 
 import java.applet.AudioClip;
+import java.awt.Color;
 import java.awt.Dimension;
 
 import java.awt.Graphics;
@@ -10,8 +11,10 @@ import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
+import javax.sound.midi.Soundbank;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -23,13 +26,13 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 
 	ScaryMaze() throws Exception {
 		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
-		maze = ImageIO.read(getClass().getResource("maze.png"));
+		maze = ImageIO.read(getClass().getResource("hi.png"));
 		//2. set the mouse pointer to the start of your maze using:
 		//new Robot().mouseMove(xPosition, yPosition)
-		
+		new Robot().mouseMove(10, 30);
 		//3. add a mouse motion listener using:
 		//addMouseMotionListener(this)
-		
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -38,15 +41,18 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
 		//4. print the mouseColor variable to see what color the mouse is touching
-
+System.out.println(mouseColor);
 		//5. make a variable to hold the background color. 
-
+int backgroundcolor = -1;
 		//6. if the mouse falls off the path (if it is on the background)
-		
+		if (mouseColor == -16777216) {
+			scare();
+		}
 				// call the scare method
 		
 		//10. if the mouse is on the end color
-				
+				if (mouseColor == -65279)
+					JOptionPane.showMessageDialog(null, "You survived death");
 				// pop up a message to tell them they won
 		
 	}
@@ -55,10 +61,11 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		System.out.println("BOO!");
 		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
 		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
-		
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
 		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
-		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+sound.play();
+		//9. drop an image into your day5 package, and use the showScaryImage method to scare your vict
+showScaryImage("keem daughter.png");
 
 	}
 
--- src/day5/StarShow.java
+++ src/day5/StarShow.java
@@ -13,15 +13,15 @@ public class StarShow {
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+		drawStar(150);	
 		// 13. Set the speed to 8
 
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+int X = robot.getX();
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+int Y = robot.getY();
 		// 8. Make a variable to hold the star size and set it to 25
-
+int size = robot.set
 		// 12. Repeat the steps #19 to #18, 30 times
 
 			// 19. Set the pen width to i 
@@ -46,13 +46,15 @@ public class StarShow {
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+robot.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
-
-			// 1. Move the robot the distance of the starSize variable
+for (int i = 0; i < 5; i++) {
 	
+}
+			// 1. Move the robot the distance of the starSize variable
+		robot.move(starSize);
 			// 3. Turn the robot 144 degrees
-			
+			robot.turn(144);
 	}
 	
 	public static void main(String[] args) {
